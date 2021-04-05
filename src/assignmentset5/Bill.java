3de7cb10bdbb92ad53e111656717369c70b055a2
510891328 <as510891328@gmail.com>
2021-04-05 13:58:09 -0400

    AssignmentSet5

---
+++ src/assignmentset5/Bill.java
@@ -0,0 +1,17 @@
+package assignmentset5;
+
+public class Bill {
+
+	public double finalPrice(int itemId) {
+		
+	}
+
+	public double finalPrice(String brandName, String itemType, int size) {
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
